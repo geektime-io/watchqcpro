8a565d4d4c9ae64cbaa4fd7738368ca7e8a46cd3
geektime-io <liupeng5296@outlook.com>
2025-11-20 20:01:55 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Watch, Activity, CheckSquare, Wifi, AlertTriangle } from 'lucide-react';
+import { Watch, Activity, CheckSquare } from 'lucide-react'; // 移除 Wifi, AlertTriangle
 import QCAlignment from './components/QCAlignment';
 import Timegrapher from './components/Timegrapher';
 import { QCMode } from './types';
